bd7770afee3bf7d59524a30c09d27c73288ac38e
Cooper Brislain <cbrislain@gmail.com>
2019-09-06 13:10:59 -0700

    committed lingering changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,40 @@
 const TriviaGame = {
     score: 0,
-    start: () => {
+    renderSplash: function() {
+        $('body').empty();
+        $splash = $('<div class="jumbotron splash">').text('Click anywhere');
+        $splash.appendTo($('body'));
+    },
+    renderQuestion: function(which) {
+        $('body').empty();
+        if (typeof which === 'undefined') {
+            which = Math.floor(Math.random()*Questions.length);
+        }
+        $('<div class="jumbotron">').appendTo($('body'))
+            .append($('<div class="row">'))
+            .append($('<div class="row">'));
+        console.log(which);
+    },
+    answerQuestion: function(which) {
+
+    },
+    start: function() {
         this.score = 0;
         this.renderSplash();
     },
-    renderSplash: () => {
-        $('body').empty();
-        $splash = $('<div class="jumbotron">').text('Click anywhere');
-        $splash.appendTo($('body'));
-    },
-    renderQuestion: (which) => {
-        $('body').empty();
-    },
-    answerQuestion: (which) => {
-
+    load: function() {
+        $.getJSON('./assets/data/questions.json')
+        .done(function(data) {
+            Questions = data;
+        });
     }
 }
 
+let Questions = [];
+
 $(document).ready(function() {
     TriviaGame.start();
+    $(document).on('click', '.splash', function() {
+        TriviaGame.renderQuestion();
+    });
 });
